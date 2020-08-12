943d11857c01704a06f3d33a15e96097f7663da8
dhirajmaan <dhirajmaanshah@gmail.com>
2020-08-12 22:16:45 +0530

    compleated

---
+++ recursion/countZero.cpp
@@ -0,0 +1,35 @@
+/*given an integer N, count the no of zeros present in the given integer using recursion*/
+#include <iostream>
+using namespace std;
+int countZero(int n)
+{
+
+    if (n <= 9)
+    {
+        if (n == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    int ans = countZero(n / 10);
+    if (n % 10 == 0)
+    {
+        ans++;
+
+        return ans;
+    }
+    else
+    {
+        return ans;
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    cout << countZero(n);
+}
